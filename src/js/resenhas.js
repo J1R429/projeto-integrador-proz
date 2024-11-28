b9bd3cb19a41032634f95f731533ec5ee6e6084d
J1R429 <jessica.fernandes@adv.oabsp.org.br>
2024-11-28 13:09:13 -0300

    css pagina resenhas 80%

---
+++ src/js/resenhas.js
@@ -0,0 +1,20 @@
+
+let currentIndex = 0;
+
+function nextItem() {
+    const items = document.querySelectorAll('.carousel-item');
+    currentIndex = (currentIndex + 1) % items.length;
+    updateCarousel();
+}
+
+function prevItem() {
+    const items = document.querySelectorAll('.carousel-item');
+    currentIndex = (currentIndex - 1 + items.length) % items.length;
+    updateCarousel();
+}
+
+function updateCarousel() {
+    const carousel = document.querySelector('.carousel');
+    const itemWidth = document.querySelector('.carousel-item').offsetWidth; // Largura do item
+    carousel.style.transform = `translateX(-${currentIndex * itemWidth}px)`; // Movimenta o carrossel para o item atual
+}
